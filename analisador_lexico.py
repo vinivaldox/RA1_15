21b9ad6a086b3151f488054186f2fa3074036db5
Vinicius Cordeiro <vinicius.cordeiro.vogt@gmail.com>
2026-03-26 20:56:03 -0300

    Adiciona a função de leitura de arquivo

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -4,3 +4,16 @@
 # Vitor Matias Percegona Bilbao | TODO: inserir usuario/apelido do github aqui
 
 # Grupo: RA1 15
+
+
+def ler_arquivo(nome_arquivo: str) -> list:
+    with open(nome_arquivo, "r", encoding="utf-8") as f:
+        linhas = [linha.strip() for linha in f.readlines() if linha.strip()]
+    return linhas
+
+
+if __name__ == "__main__":
+    nome_arquivo = "teste_1.txt"
+    linhas = ler_arquivo(nome_arquivo)
+    for linha in linhas:
+        print(linha)
