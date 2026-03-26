19cf792a20109d77af9b0d1d26eba6478e8500c0
Vinicius Cordeiro <vinicius.cordeiro.vogt@gmail.com>
2026-03-26 20:27:45 -0300

    Adiciona o header do arquivo fonte

---
+++ analisador_lexico.py
@@ -0,0 +1,6 @@
+# Nome | apelido no Github | link do Github
+# Andrei de Carvalho Bley | TODO: inserir usuario/apelido do github aqui
+# Vinicius Cordeiro Vogt | vinivaldox | https://github.com/vinivaldox
+# Vitor Matias Percegona Bilbao | TODO: inserir usuario/apelido do github aqui
+
+# Grupo: RA1 15
