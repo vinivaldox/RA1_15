f0e380f07de2ca57673fee984916cd597cc143f9
Vitor <vitaodamassa35@gmail.com>
2026-03-26 23:40:54 -0300

    editei as minhas infoemações la em cima :/

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,6 +1,6 @@
 # Nome | apelido no Github | link do Github
 # Andrei de Carvalho Bley | TODO: inserir usuario/apelido do github aqui
 # Vinicius Cordeiro Vogt | vinivaldox | https://github.com/vinivaldox
-# Vitor Matias Percegona Bilbao | TODO: inserir usuario/apelido do github aqui
+# Vitor Matias Percegona Bilbao | vitormpbilbao | https://github.com/vitormpbilbao
 
 # Grupo: RA1 15
