c76a7ee7bf4d29686e1c854ddc8bc13f2b69f818
Andrei Bley <andreicb101@gmail.com>
2026-03-27 00:36:05 -0300

    monta a subexpressão de 3 valores com dicionário

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 #testando com token manual, sempre que ver ")" na expressao
 tokens = ["(", "3.14", "2.0", "+", ")"]
 tokens2 = ["(", "11.8", "2,0", "%", ")"]
+tokens3 =["(", "(", "1", "2", "+", ")", "3", "/", ")"]
 
 #pega os token e adiciona numa pilha até encontrar ")", que significa que a operação acabou, então desempilha em uma lista até achar o "("
 #que significa que chegou no início da expressão
@@ -19,6 +20,22 @@ def executarExpressao(tokens):
             pilha.pop()  #remove "("
             elementos.reverse() #corrige ordem
 
-            print("Expressão encontrada:", elementos)
+            if len(elementos) == 3:
+                op1 = elementos[0]
+                op2 = elementos[1]
+                op = elementos[2]
+
+                subexpressao = {        #dicionario da operação
+                    "op1": op1,     #operante1
+                    "op2": op2,     #operante2
+                    "op": op        #operador
+                }
+
+                pilha.append(subexpressao)
+                
+            else:
+                print("ERRO: subexpressão inválida: ", elementos)
 
     return pilha
+
+print(executarExpressao(tokens3))
