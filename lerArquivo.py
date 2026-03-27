03bb5a9879e098f2248829d258894b66d8a7f826
Vitor <scartsure25@gmail.com>
2026-03-26 23:03:05 -0300

    adiciona uma função para ler arquivo

---
+++ lerArquivo.py
@@ -0,0 +1,10 @@
+def lerArquivo(nomeArquivo):
+    try:
+        with open(nomeArquivo, 'teste_1.txt') as arquivo:
+            #le semquebra de linha
+            linhas = [linha.strip() for linha in arquivo.readlines()]
+            print(f"Arquivo '{nomeArquivo}' lido com sucesso!")
+            return linhas
+    except FileNotFoundError:
+        print(f"Erro: Não foi possível abrir o arquivo '{nomeArquivo}'. Verifique o caminho.")
+        return []
