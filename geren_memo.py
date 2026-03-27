dcc901750ed4a0d4ab8e0b1509c8df439638bd6a
Andrei Bley <andreicb101@gmail.com>
2026-03-27 14:11:16 -0300

    inicio da implementacao de memoria e resultado

--- geren_memo.py
+++ geren_memo.py
@@ -7,13 +7,18 @@ tokenfalho = ["1", "2", "+", ")"] #teste faltando parentesis
 tokenfalho2 = ["(", "1", "+", ")"]#teste faltando operando (2 elementos)
 tokenfalho3 = ["(", "1", "2", "3", "+", ")"]#teste com elementos demais
 tokenfalho4 = ["(", "1", "2", "&", ")"] #teste operador invalido
-tokenfalho5 = ["(", "+", "1", "2", ")"] #ordem errada <- *********** ainda precisa validar os op1, op2, op***********
+tokenfalho5 = ["(", "+", "1", "2", ")"] 
+
+tokensMEM = ["(", "MEM", "2", "+", ")"]
+tokensRES = ["(", "5", "RES", "*", ")"]
+
+tokensdefMEM = ["(", "2", "MEM", ")"]
 
 
 
 #pega os token e adiciona numa pilha até encontrar ")", que significa que a operação acabou, então desempilha em uma lista até achar o "("
 #que significa que chegou no início da expressão
-def executarExpressao(tokens):
+def executarExpressao(tokens, memoria, resultados):
     pilha = []
     operadores = ["+", "-", "*", "/", "//", "%", "^"]
 
@@ -33,6 +38,27 @@ def executarExpressao(tokens):
             pilha.pop()  #remove 1 "("
             elementos.reverse() #corrige ordem
 
+            # caso (N RES)
+            if len(elementos) == 2 and elementos[1] == "RES":
+                subexpressao = {
+                    "tipo": "res_get",
+                    "indice": elementos[0]
+                }
+                pilha.append(subexpressao)
+                continue
+
+            # caso (V MEM)
+            if len(elementos) == 2 and elementos[1] == "MEM":
+                subexpressao = {
+                    "tipo": "mem_set",
+                    "valor": elementos[0],
+                    "memoria": "MEM"
+                }
+                pilha.append(subexpressao)
+                continue 
+        
+
+
             if len(elementos) != 3:                         #validação se segue o formato de -> operando1, operando2, operador
                 print("ERRO: subexpressão inválida: ", elementos)
                 return None
@@ -47,6 +73,7 @@ def executarExpressao(tokens):
                 return None
 
             subexpressao = {        #dicionario da operação
+                "tipo": "operacao",
                 "op1": op1,     #operando1
                 "op2": op2,     #operando2
                 "op": op        #operador
@@ -60,4 +87,9 @@ def executarExpressao(tokens):
 
     return pilha[0]
 
-print(executarExpressao(tokens2))
+
+print(executarExpressao(tokens, {}, []))
+print(executarExpressao(tokens2, {}, []))
+print(executarExpressao(tokensRES, {}, []))
+print(executarExpressao(tokensMEM, {}, []))
+print(executarExpressao(tokensdefMEM, {}, []))
