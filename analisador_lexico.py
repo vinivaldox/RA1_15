aa8b8a043b24ab9be471f2a098234259d05dbfd6
vito <vitaodamassa35@gmail.com>
2026-03-27 21:08:44 -0300

    coloquei meu nome no seu arquivo vinizin

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,7 +1,7 @@
 # Nome | apelido no Github | link do Github
 # Andrei de Carvalho Bley | TODO: inserir usuario/apelido do github aqui
 # Vinicius Cordeiro Vogt | vinivaldox | https://github.com/vinivaldox
-# Vitor Matias Percegona Bilbao | TODO: inserir usuario/apelido do github aqui
+# Vitor Matias Percegona Bilbao | vitormpbilbao | https://github.com/vitormpbilbao
 
 # Grupo: RA1 15
 from dataclasses import dataclass
